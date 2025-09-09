4eacf478122e75777262f62a8eb58d4116bd76d8
Adam Smedberg <asmedberg@gmail.com>
2025-09-08 17:56:49 -0700

    jump on click added

--- sketch.js
+++ sketch.js
@@ -1,16 +1,51 @@
+// variables
+let isJumping = false;
+let jumpTime = 0;
+const jumpDuration = 60;
+const jumpHeight = 100;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  screen();
+  scene();
   player();
+
+  // update jump physics
+  if (isJumping) {
+    jumpTime++;
+    if (jumpTime >= jumpDuration) {
+      isJumping = false;
+      jumpTime = 0;
+    }
+  }
+}
+
+function scene() {
+  strokeWeight(1);
+  stroke(0, 0, 0);
+  line(0, height - 87, width, height - 88);
 }
 
 function player() {
+  let yPos = height - 100; // default y position
+
+  if (isJumping) {
+    let jumpOffset = sin((jumpTime / jumpDuration) * PI) * jumpHeight;
+    yPos -= jumpOffset;
+  }
+
   // start player at bottom left of canvas
   noStroke();
   fill(0, 0, 0);
-  circle(50, height - 100, 25);
+  circle(50, yPos, 24);
+}
+
+function mouseClicked() {
+  if (!isJumping) {
+    isJumping = true;
+    jumpTime = 0;
+  }
 }
